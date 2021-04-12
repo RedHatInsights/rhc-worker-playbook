5fb6f0e8cd6d110a3b65fdcb801d2a664989ff9a
Jeremy Crafts <jcrafts@redhat.com>
2021-04-12 17:10:13 +0000

    revert mistaken changes

--- rhc_worker_playbook/constants.py
+++ rhc_worker_playbook/constants.py
@@ -1,6 +1,6 @@
 import os
-WORKER_LIB_DIR = "/usr/lib64/rhc-worker-playbook"
-CONFIG_FILE = "/etc/rhc/workers/rhc-worker-playbook.toml"
+WORKER_LIB_DIR = os.path.join(os.path.dirname(__file__), "contrib")
+CONFIG_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "rhc-worker-playbook.toml")
 NEWEST_EGG = os.path.join(os.sep, "var", "lib", "insights", "newest.egg")
 STABLE_EGG = os.path.join(os.sep, "var", "lib", "insights", "last_stable.egg")
 RPM_EGG = os.path.join(os.sep, "etc", "insights-client", "rpm.egg")
