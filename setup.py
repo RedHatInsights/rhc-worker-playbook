5dea52bca8485b46606bf58268bc70c872211582
Link Dupont <link@sub-pop.net>
2021-08-26 12:41:06 -0400

    chore: set version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rhc-worker-playbook",
-    version="0.1.5",
+    version="0.1.6",
     author="Jeremy Crafts",
     author_email="jcrafts@redhat.com",
     description="Python worker for RHC",
