b43b59cca65e81e969e34c619f52e94fc08623e9
Jiri Hnidek <jhnidek@redhat.com>
2024-01-26 11:02:31 +0100

    fix: Small improvements of develop setup script.

--- develop-setup.sh
+++ develop-setup.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 CONTRIB_DIR=$(pwd -P)/rhc_worker_playbook/contrib
 
-mkdir $CONTRIB_DIR
-python3 -m pip install --target $CONTRIB_DIR -r test-requirements.txt
+mkdir "${CONTRIB_DIR}"
+python3 -m pip install --target "${CONTRIB_DIR}" -r test-requirements.txt
