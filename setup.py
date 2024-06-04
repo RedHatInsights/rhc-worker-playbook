64d1c92846edb83b065fbc056c89b4e2f3303932
Link Dupont <link@sub-pop.net>
2024-06-04 12:53:12 -0400

    chore: set version to 0.1.9
    Set the version to 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rhc-worker-playbook",
-    version="0.1.8",
+    version="0.1.9",
     author="Jeremy Crafts",
     author_email="jcrafts@redhat.com",
     description="Python worker for RHC",
