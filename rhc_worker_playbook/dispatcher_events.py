f1195cebbaac0a6ebcb52996409b733206488179
Link Dupont <link@sub-pop.net>
2024-06-05 08:39:22 -0400

    style: format files with black and fix warnings
    Format files using black, fix legitimate linting warnings, ignoring a couple that we regularly violate.

--- rhc_worker_playbook/dispatcher_events.py
+++ rhc_worker_playbook/dispatcher_events.py
@@ -1,6 +1,7 @@
-'''
+"""
 Event-generating functions for the Playbook Dispatcher
-'''
+"""
+
 import uuid
 
 
@@ -13,14 +14,14 @@ def executor_on_start(correlation_id=None, stdout=None):
         "stdout": stdout,
         "start_line": 0,
         "end_line": 0,
-        "event_data": {
-            "crc_dispatcher_correlation_id": correlation_id
-        }
+        "event_data": {"crc_dispatcher_correlation_id": correlation_id},
     }
 
 
 # required event for cloud connector
-def executor_on_failed(correlation_id=None, stdout=None, error_code=None, error_details=None):
+def executor_on_failed(
+    correlation_id=None, stdout=None, error_code=None, error_details=None
+):
     return {
         "event": "executor_on_failed",
         "uuid": str(uuid.uuid4()),
@@ -31,6 +32,6 @@ def executor_on_failed(correlation_id=None, stdout=None, error_code=None, error_
         "event_data": {
             "crc_dispatcher_correlation_id": correlation_id,
             "crc_dispatcher_error_code": error_code,
-            "crc_dispatcher_error_details": error_details
-        }
+            "crc_dispatcher_error_details": error_details,
+        },
     }
