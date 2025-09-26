3de74e79906fc169b254c56ff43f58e1d2f01092
Jeremy Crafts <jcrafts@redhat.com>
2025-09-26 13:11:27 -0400

    refactor(RHINENG-20955): add verifier call, drop insights-client relation

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,21 +3,16 @@ package config
 import "time"
 
 const (
-	FlagNameDirective            = "directive"
-	FlagNameInsightsCoreGPGCheck = "insights-core-gpg-check"
-	FlagNameLogLevel             = "log-level"
-	FlagNameVerifyPlaybook       = "verify-playbook"
-	FlagNameResponseInterval     = "response-interval"
-	FlagNameBatchEvents          = "batch-events"
+	FlagNameDirective        = "directive"
+	FlagNameLogLevel         = "log-level"
+	FlagNameVerifyPlaybook   = "verify-playbook"
+	FlagNameResponseInterval = "response-interval"
+	FlagNameBatchEvents      = "batch-events"
 )
 
 type Config struct {
 	// Directive is the worker destination name to register with yggdrasil.
 	Directive string
-
-	// InsightsCoreGPGCheck determines whether or not to perform GPG
-	// verification on insights-core.egg.
-	InsightsCoreGPGCheck bool
 
 	// LogLevel is the level value used for logging.
 	LogLevel string
@@ -38,10 +33,9 @@ type Config struct {
 // DefaultConfig is a globally accessible Config data structure, initialized
 // with default values.
 var DefaultConfig = Config{
-	Directive:            "rhc_worker_playbook",
-	InsightsCoreGPGCheck: true,
-	LogLevel:             "error",
-	VerifyPlaybook:       true,
-	ResponseInterval:     0,
-	BatchEvents:          0,
+	Directive:        "rhc_worker_playbook",
+	LogLevel:         "error",
+	VerifyPlaybook:   true,
+	ResponseInterval: 0,
+	BatchEvents:      0,
 }
