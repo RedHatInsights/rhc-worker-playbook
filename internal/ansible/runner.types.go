79f09afc66f56d20a3508d0e14547dfac1551615
Jeremy Crafts <jcrafts@redhat.com>
2025-10-14 12:23:57 -0400

    chore(RHINENG-18785): address feedback, send full event if filtering fails

--- internal/ansible/runner.types.go
+++ internal/ansible/runner.types.go
@@ -3,48 +3,50 @@
 
 package ansible
 
-type PlaybookRunResponseMessageYamlEventsElem struct {
+// Overall structure of the data in an ansible job event needed by playbook dispatcher
+type PlaybookRunResponseMessageEventsElem struct {
 	// Counter corresponds to the JSON schema field "counter".
-	Counter int `json:"counter" yaml:"counter" mapstructure:"counter"`
+	Counter int `json:"counter"`
 
 	// EndLine corresponds to the JSON schema field "end_line".
-	EndLine int `json:"end_line" yaml:"end_line" mapstructure:"end_line"`
+	EndLine int `json:"end_line"`
 
 	// Event corresponds to the JSON schema field "event".
-	Event string `json:"event" yaml:"event" mapstructure:"event"`
+	Event string `json:"event"`
 
 	// EventData corresponds to the JSON schema field "event_data".
-	EventData *PlaybookRunResponseMessageYamlEventsElemEventData `json:"event_data,omitempty" yaml:"event_data,omitempty" mapstructure:"event_data,omitempty"`
+	EventData *PlaybookRunResponseMessageEventsElemEventData `json:"event_data,omitempty"`
 
 	// StartLine corresponds to the JSON schema field "start_line".
-	StartLine int `json:"start_line" yaml:"start_line" mapstructure:"start_line"`
+	StartLine int `json:"start_line" yaml:"start_line"`
 
 	// Stdout corresponds to the JSON schema field "stdout".
-	Stdout *string `json:"stdout,omitempty" yaml:"stdout,omitempty" mapstructure:"stdout,omitempty"`
+	Stdout *string `json:"stdout,omitempty"`
 
 	// Uuid corresponds to the JSON schema field "uuid".
-	Uuid string `json:"uuid" yaml:"uuid" mapstructure:"uuid"`
+	Uuid string `json:"uuid"`
 }
 
-type PlaybookRunResponseMessageYamlEventsElemEventData struct {
+// Structure of the "event_data" field in an ansible job event needed by playbook dispatcher
+type PlaybookRunResponseMessageEventsElemEventData struct {
 	// CrcDispatcherCorrelationId corresponds to the JSON schema field
 	// "crc_dispatcher_correlation_id".
-	CrcDispatcherCorrelationId *string `json:"crc_dispatcher_correlation_id,omitempty" yaml:"crc_dispatcher_correlation_id,omitempty" mapstructure:"crc_dispatcher_correlation_id,omitempty"`
+	CrcDispatcherCorrelationId *string `json:"crc_dispatcher_correlation_id,omitempty"`
 
 	// CrcDispatcherErrorCode corresponds to the JSON schema field
 	// "crc_dispatcher_error_code".
-	CrcDispatcherErrorCode *string `json:"crc_dispatcher_error_code,omitempty" yaml:"crc_dispatcher_error_code,omitempty" mapstructure:"crc_dispatcher_error_code,omitempty"`
+	CrcDispatcherErrorCode *string `json:"crc_dispatcher_error_code,omitempty"`
 
 	// CrcDispatcherErrorDetails corresponds to the JSON schema field
 	// "crc_dispatcher_error_details".
-	CrcDispatcherErrorDetails *string `json:"crc_dispatcher_error_details,omitempty" yaml:"crc_dispatcher_error_details,omitempty" mapstructure:"crc_dispatcher_error_details,omitempty"`
+	CrcDispatcherErrorDetails *string `json:"crc_dispatcher_error_details,omitempty"`
 
 	// Host corresponds to the JSON schema field "host".
-	Host *string `json:"host,omitempty" yaml:"host,omitempty" mapstructure:"host,omitempty"`
+	Host *string `json:"host,omitempty"`
 
 	// Playbook corresponds to the JSON schema field "playbook".
-	Playbook *string `json:"playbook,omitempty" yaml:"playbook,omitempty" mapstructure:"playbook,omitempty"`
+	Playbook *string `json:"playbook,omitempty"`
 
 	// PlaybookUuid corresponds to the JSON schema field "playbook_uuid".
-	PlaybookUuid *string `json:"playbook_uuid,omitempty" yaml:"playbook_uuid,omitempty" mapstructure:"playbook_uuid,omitempty"`
+	PlaybookUuid *string `json:"playbook_uuid,omitempty"`
 }
