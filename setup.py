3bd87046be42607c987d530bf22f8e9f8f025fae
Jeremy Crafts <jcrafts@redhat.com>
2026-02-04 21:38:56 -0500

    build: set version to 0.1.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="rhc-worker-playbook",
-    version="0.1.13",
+    version="0.1.14",
     author="Jeremy Crafts",
     author_email="jcrafts@redhat.com",
     description="Python worker for RHC",
